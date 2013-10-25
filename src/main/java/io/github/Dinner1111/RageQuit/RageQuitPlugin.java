08b63793db1e09d3a5c336b5b892e4cff92e8450
Dinner1111 <mr.pepper@aol.com>
2013-10-24 22:59:56 -0400

    Redo some things

--- src/main/java/io/github/Dinner1111/RageQuit/RageQuitPlugin.java
+++ src/main/java/io/github/Dinner1111/RageQuit/RageQuitPlugin.java
@@ -7,76 +7,77 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RageQuitPlugin extends JavaPlugin implements CommandExecutor {
-    int task;
-    int n = 0;
-    @Override
-    public void onEnable() {
-        getLogger().info("Time to rage!");
-    }
-    @Override
-    public void onDisable() {
-        getLogger().info("Had enough rage?");
-    }
-    public boolean onCommand(CommandSender sender, Command cmd, String cmdLine, String[] args) {
-        boolean canRQ = true;
-        if (cmd.getName().equalsIgnoreCase("rq")) {
-            if (sender.hasPermission("RageQuit.Rage")) {
-                if (sender instanceof Player) {
-                    if (args.length == 1 ) {
-                        if (args[0].equalsIgnoreCase("-c") && sender.hasPermission("RageQuit.Rage.Console")) {
-                            canRQ = false;
-                            consoleRQ();
-                        } else {
-                            sender.sendMessage(ChatColor.DARK_RED + "Invalid flag.");
-                            sender.sendMessage(ChatColor.DARK_RED + "Usage: /rq [-c]");
-                        }
-                    } else {
-                        sender.sendMessage(ChatColor.DARK_RED + "Too many arguments.");
-                        sender.sendMessage(ChatColor.DARK_RED + "Usage: /rq [-c]");
-                    }
-                    Player p = (Player) sender;
-                    if (canRQ == true) {
-                        getServer().broadcastMessage(ChatColor.AQUA + p.getName() + ChatColor.GRAY + " has " + ChatColor.DARK_RED + "rage quit" + ChatColor.GRAY + "!");
-                        p.kickPlayer(ChatColor.GRAY + "You " + ChatColor.DARK_RED + "rage quit" + ChatColor.GRAY + "!");
-                        getLogger().info(p.getName() + " rage quit. Hah.");
-                        
-                    }
-                    return true;
-                } else {
-                    consoleRQ();
-                }
-            }
-        }
-        return true;
-    }
-    public void consoleRQ() {
-        task = Bukkit.getServer().getScheduler()
-                .scheduleSyncRepeatingTask(this, new Runnable() {
-                    public void run() {
-                        switch (n) {
-                        case 0:
-                            getServer().broadcastMessage(ChatColor.GRAY + "Oh no! " + ChatColor.DARK_RED + "Console " + ChatColor.GRAY + "seems to be building up rage!");
-                            n = n + 1;
-                            break;
-                        case 1:
-                            getServer().broadcastMessage(ChatColor.GRAY + "Oh my... " + ChatColor.DARK_RED + "Console" + ChatColor.GRAY + " is boiling with rage...");
-                            n = n + 1;
-                            break;
-                        case 2:
-                            getServer().broadcastMessage(ChatColor.GRAY + "OH NO, EVERYONE RUN," + ChatColor.DARK_RED + " CONSOLE" + ChatColor.GRAY + " IS GOING TO RAGE!!!");
-                            n = n + 1;
-                            break;
-                        case 3:
-                            getServer().broadcastMessage(ChatColor.GRAY + "NOOOOOOOOO " + ChatColor.DARK_RED + "CONSOLE" + ChatColor.GRAY + " HAS RAGE QUIT!!!!!");
-                            n = n + 1;
-                            break;
-                        case 4:
-                            for (Player p : Bukkit.getOnlinePlayers()) {
-                                p.kickPlayer(ChatColor.DARK_RED + "Console " + ChatColor.GRAY + "has rage quit!");
-                                Bukkit.getServer().shutdown();
-                            }
-                        }
-                    }
-                }, 0, 40);
-    }
+	int task;
+	int n = 0;
+	@Override
+	public void onEnable() {
+		getLogger().info("Time to rage!");
+	}
+	@Override
+	public void onDisable() {
+		getLogger().info("Had enough rage?");
+	}
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdLine, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("rq")) {
+			if (sender.hasPermission("RageQuit.Rage")) {
+				if (sender instanceof Player) {
+					if (args.length == 0) {
+						Player p = (Player) sender;
+						getServer().broadcastMessage(ChatColor.AQUA + p.getDisplayName() + ChatColor.GRAY + " has " + ChatColor.AQUA + "rage quit" + ChatColor.GRAY + "!");
+						p.kickPlayer(ChatColor.GRAY + "You " + ChatColor.AQUA + "rage quit" + ChatColor.GRAY + "!");
+						getLogger().info(p.getDisplayName() + " rage quit. Hah.");
+						return true;
+					}
+					if (args.length == 1) {
+						if (args[0].equalsIgnoreCase("-c") && sender.hasPermission("RageQuit.ConsoleRage")) {
+							consoleRQ();
+							return true;
+						} else {
+							sender.sendMessage(ChatColor.GRAY + "Invalid flag.");
+							sender.sendMessage(ChatColor.GRAY + "Usage: " + ChatColor.AQUA + "/rq [-c]");
+							return true;
+						}
+					} else {
+						sender.sendMessage(ChatColor.GRAY + "Too many arguments.");
+						sender.sendMessage(ChatColor.GRAY + "Usage: " + ChatColor.AQUA + "/rq [-c]");
+						return true;
+					}
+				} else {
+					consoleRQ();
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public void consoleRQ() {
+		task = Bukkit.getServer().getScheduler()
+				.scheduleSyncRepeatingTask(this, new Runnable() {
+					public void run() {
+						switch (n) {
+						case 0:
+							getServer().broadcastMessage(ChatColor.GRAY + "Oh no! " + ChatColor.GOLD + "*" + ChatColor.RED + "Console" + ChatColor.GRAY + " seems to be building up rage!");
+							n = n + 1;
+							break;
+						case 1:
+							getServer().broadcastMessage(ChatColor.GRAY + "Oh my... " + ChatColor.GOLD + "*" + ChatColor.RED + "Console" + ChatColor.GRAY + " is boiling with rage...");
+							n = n + 1;
+							break;
+						case 2:
+							getServer().broadcastMessage(ChatColor.GRAY + "OH NO, EVERYONE RUN, " + ChatColor.GOLD + "*" + ChatColor.RED + "Console" + ChatColor.GRAY + " IS GOING TO RAGE!!!");
+							n = n + 1;
+							break;
+						case 3:
+							getServer().broadcastMessage(ChatColor.GRAY + "NOOOOOOOOO " + ChatColor.GOLD + "*" + ChatColor.RED + "Console" + ChatColor.GRAY + " HAS RAGE QUIT!!!!!");
+							n = n + 1;
+							break;
+						case 4:
+							for (Player p : Bukkit.getOnlinePlayers()) {
+								p.kickPlayer(ChatColor.DARK_RED + "Console " + ChatColor.GRAY + "has rage quit!");
+								Bukkit.getServer().shutdown();
+							}
+						}
+					}
+				}, 0, 40);
+	}
 }
